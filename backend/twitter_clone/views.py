e91b6cebe500ec5c2e4346e977503f93c3c70031
Ricardo Ferreira <jrjunior72@hotmail.com>
2025-12-03 01:19:58 -0300

    feat: #2 configuração do deploy atuomático.

--- backend/twitter_clone/views.py
+++ backend/twitter_clone/views.py
@@ -13,7 +13,7 @@ def update(request):
         stored on PythonAnywhere in the git.Repo() as parameter.
         Here the name of my directory is "test.pythonanywhere.com"
         '''
-        repo = git.Repo('/home/ricardoferreirajr/twitter-clone-fixed/backend')
+        repo = git.Repo('/home/ricardoferreirajr/twitter-clone-fixed/backend/twitter_clone')
         origin = repo.remotes.origin
 
         origin.pull()
