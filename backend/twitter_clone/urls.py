7ed485ed69a1454c9150a5ffc71e49cf4540bbb7
Ricardo Ferreira <jrjunior72@hotmail.com>
2025-12-03 00:57:29 -0300

    feat: configuração do deploy atuomático.

--- backend/twitter_clone/urls.py
+++ backend/twitter_clone/urls.py
@@ -4,12 +4,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from twitter_clone import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('users.urls')), # ⬅️ rotas do app users
     path('api/posts/', include('posts.urls')), # ⬅️ rotas do app posts
     path('api/relationships/', include('relationships.urls')), 
+    path("update_server/", views.update, name="update"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
